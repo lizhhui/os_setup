e0fb644bc233c039512fe0dcf9d6e72e674cf1cd
server <admin@lizhanghui.com>
2020-01-03 21:17:54 +0800

    from mac

--- needed_package_install.bash
+++ needed_package_install.bash
@@ -8,41 +8,41 @@ echo "4 > add desktop env ."
 ###################################
 #    add source list
 ###################################
-#安装add-apt-repository命令
-apt-get install software-properties-common
-#增加 cinnamon软件源
-add-apt-repository ppa:lestcape/cinnamon
-#增加 mate desktop软件源
-apt-add-repository ppa:ubuntu-mate-dev/ppa
-apt-add-repository ppa:ubuntu-mate-dev/trusty-mate
+##安装add-apt-repository命令
+#apt-get install software-properties-common
+##增加 cinnamon软件源
+#add-apt-repository ppa:lestcape/cinnamon
+##增加 mate desktop软件源
+#apt-add-repository ppa:ubuntu-mate-dev/ppa
+#apt-add-repository ppa:ubuntu-mate-dev/trusty-mate
 
 ###################################
 #    apt updata/upgrade
 ###################################
 #更新软件源与升级软件
 apt-get update
-apt-get upgrade
+#apt-get upgrade
 
 
 ###################################
 #    tcsh svn emacs git ...
 ###################################
-  CMD_EXIST=`which git`
-if [ -z $CMD_EXIST ] ; then
-  apt-get update
-  apt-get upgrade
-  sudo apt-get install tcsh
-  sudo apt-get install subversion
-  sudo apt-get install emacs24
-  sudo apt-get install git
-  sudo apt-get install dos2unix
-fi
+  sudo apt-get install tcsh -y
+  sudo apt-get install subversion -y
+  sudo apt-get install emacs24 -y
+  sudo apt-get install git -y
+  sudo apt-get install dos2unix -y
+
+###################################
+# lmgrd出现no such file and directory问题
+###################################
+  sudo apt-get install lsb-core -y
 
 ###################################
 #    desktop env install...
 ###################################
 #若要安装mate desktop 环境
-sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop
+#sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop
 
 #若要安装cinnamon环境
 #sudo apt-get install cinnamon
